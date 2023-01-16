cf282e1d02b2fec7974d7ea61bc170be4e322de4
shagan-plaatjies <shagan@bbd.co.za>
2023-01-16 22:23:08 +0200

    initial pseudocode

---
+++ cloning.tsx
@@ -0,0 +1,19 @@
+// Starting: C, M, S, H
+// Step 0: C, M, S, H
+// Step 1: M, S, H, C, C
+// Step 2: S, H, C, C, M, M
+// Step 3: H, C, C, M, M, S, S
+// Step 4: C, C, M, M, S, S, H, H
+// Step 5: C, M, M, S, S, H, H, C, C
+// Step 6: M, M, S, S, H, H, C, C, C, C
+
+// Parse Input (2 lines, first is list, second is n)
+//     Create Array from Input
+//      Store N integer
+
+// Loop through N times
+//      Pop from front
+//      Append to back, twice
+
+// print last element in queue
+// print all elements in queue
